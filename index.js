86d6099f52197ab99e5a3c45e5d1dafba0823583
Luong Trung Thanh <thnhlngtrung@gmail.com>
2020-12-07 11:18:17 +0700

    hihi

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
+
+app.get('/', function (req, res) {
+  
+
+    res.send("Xin chao tat ca cac ban");
+  });
+
+
 //config connect db
-var mysql = require('mysql');
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "japanese"
-});
+// var mysql = require('mysql');
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "japanese"
+// });
 
 
 //test connect db
@@ -20,43 +28,43 @@ var con = mysql.createConnection({
 
 
 //access table course
-app.get('/api/course', function (req, res) {
-  var sql = "SELECT * FROM course";
-  con.query(sql, function(err, results) {
-    if (err) throw err;
-    res.json(results);
-  });
-});
+// app.get('/api/course', function (req, res) {
+//   var sql = "SELECT * FROM course";
+//   con.query(sql, function(err, results) {
+//     if (err) throw err;
+//     res.json(results);
+//   });
+// });
 
 //get detail course with id
-app.get('/api/course/:id', (req,res) => {
-	let id = req.params.id ;
-	var sql = "SELECT id,jWord,vnWord,imgWord FROM detailcourse WHERE idCourse=" + id;
-	con.query(sql, function(err, results) {
-    if (err) throw err;
-    res.json(results);
-  });
-});
+// app.get('/api/course/:id', (req,res) => {
+// 	let id = req.params.id ;
+// 	var sql = "SELECT id,jWord,vnWord,imgWord FROM detailcourse WHERE idCourse=" + id;
+// 	con.query(sql, function(err, results) {
+//     if (err) throw err;
+//     res.json(results);
+//   });
+// });
 
 //get all vocabulary
-app.get('/api/vocabulary', function (req, res) {
-  var sql = "SELECT * FROM vocabulary";
-  con.query(sql, function(err, results) {
-    if (err) throw err;
-    res.json(results);
-  });
-});
+// app.get('/api/vocabulary', function (req, res) {
+//   var sql = "SELECT * FROM vocabulary";
+//   con.query(sql, function(err, results) {
+//     if (err) throw err;
+//     res.json(results);
+//   });
+// });
 
 //get detail vocabulary with id
-app.get('/api/vocabulary/:id', (req,res) => {
-	let id = req.params.id ;
-	var sql = "SELECT dv.id,jWord,vnWord,tw.name,tw.shortName FROM detailvocabulary as dv, typeword AS tw WHERE typeWord=tw.id and idVocabulary=" 
-	+ id + " ORDER BY dv.id;" ;
-	con.query(sql, function(err, results) {
-    if (err) throw err;
-    res.json(results);
-  });
-});
+// app.get('/api/vocabulary/:id', (req,res) => {
+// 	let id = req.params.id ;
+// 	var sql = "SELECT dv.id,jWord,vnWord,tw.name,tw.shortName FROM detailvocabulary as dv, typeword AS tw WHERE typeWord=tw.id and idVocabulary=" 
+// 	+ id + " ORDER BY dv.id;" ;
+// 	con.query(sql, function(err, results) {
+//     if (err) throw err;
+//     res.json(results);
+//   });
+// });
 
 app.listen(port,function(){
     console.log('Node server running @ http://localhost:' + port)
